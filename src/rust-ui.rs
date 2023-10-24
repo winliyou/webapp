a70c54d63d6f6cd2bad8eb56d91832533f5d1fdb
you.li <you.li@nokia-sbell.com>
2023-10-24 16:05:43 +0300

    get input value by JsCast

--- src/rust-ui.rs
+++ src/rust-ui.rs
@@ -1,48 +1,98 @@
-use std::collections::HashMap;
+use wasm_bindgen::{JsCast, JsValue};
+use yew::prelude::*;
+struct App {
+    current_random_number: u32,
+    guessed_number: u32,
+}
 
-use web_sys::console;
-struct App {}
-
-enum MyMessage {}
+enum MyMessage {
+    MsgGuessUpdate(u32),
+    MsgGuessTry,
+}
 
 impl yew::Component for App {
     type Message = MyMessage;
 
     type Properties = ();
     fn create(ctx: &yew::Context<Self>) -> Self {
-        App {}
-    }
-    fn changed(&mut self, ctx: &yew::Context<Self>, _old_props: &Self::Properties) -> bool {
-        true
+        let rand_number: u32 = rand::random::<u32>() % 1000;
+        web_sys::console::log_1(&JsValue::from(format!("random: {}", rand_number)));
+        App {
+            current_random_number: rand_number,
+            guessed_number: 0,
+        }
     }
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
-        let mut mymap: HashMap<&str, i32> = HashMap::new();
-        mymap.insert("hello", rand::random());
-        mymap.insert("world", rand::random());
-        mymap.insert("asdf", rand::random());
-        mymap.insert("bas", rand::random());
-
         yew::html! {
-        <div>
-            {
-                mymap
-                .iter()
-                .map(|p| {
-                    yew::html! {
-                        <div>
-                        <h1>{p.0}</h1>
-                        <h2>{p.1}</h2>
-                        </div>
-                    }
-                })
-                .collect::<yew::Html>()
-            }
-        </div>}
+            <div>
+                <input
+                    type="number"
+                    placeholder={"请输入0-1000中间的一个数"}
+                    oninput={
+                        ctx.link().callback(
+                            |e: InputEvent| {
+                                if let Some(target) = e.target()
+                                {
+                                    if let Ok(input_element) = target.dyn_into::<web_sys::HtmlInputElement>()
+                                    {
+                                        if let Ok(num) = input_element.value().parse::<u32>()
+                                        {
+                                            return MyMessage::MsgGuessUpdate(num);
+                                        }
+                                    }
+                                }
+                                MyMessage::MsgGuessUpdate(0)
+                            }
+                        )}
+                    />
+                <button name="guess"
+                    onclick={
+                        ctx.link().callback(
+                            |_|{
+                                web_sys::console::log_1(&JsValue::from("clicked")); MyMessage::MsgGuessTry
+                            }
+                        )}
+                    >
+                    {"确定"}
+                    </button>
+            </div>
+        }
     }
-    fn rendered(&mut self, ctx: &yew::Context<Self>, first_render: bool) {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            MyMessage::MsgGuessTry => match self.guessed_number {
+                x if (x < self.current_random_number) => {
+                    web_sys::console::log_1(&JsValue::from(format!(
+                        "target number: {} , guessed number: {}",
+                        self.current_random_number, self.guessed_number
+                    )));
+                    web_sys::console::log_1(&JsValue::from("Less"));
+                }
+                x if (x > self.current_random_number) => {
+                    web_sys::console::log_1(&JsValue::from(format!(
+                        "target number: {} , guessed number: {}",
+                        self.current_random_number, self.guessed_number
+                    )));
+                    web_sys::console::log_1(&JsValue::from("Great"));
+                }
+                x if (x == self.current_random_number) => {
+                    web_sys::console::log_1(&JsValue::from(format!(
+                        "target number: {} , guessed number: {}",
+                        self.current_random_number, self.guessed_number
+                    )));
+                    web_sys::console::log_1(&JsValue::from("You Are Right"));
+                }
+                _ => {
+                    web_sys::console::log_1(&JsValue::from("What's Your Problem"));
+                }
+            },
+            MyMessage::MsgGuessUpdate(x) => {
+                self.guessed_number = x;
+            }
+        }
+        true
     }
 }
-
 fn main() {
     yew::Renderer::<App>::with_root(
         web_sys::window()
